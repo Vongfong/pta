4c606108a0acbdfcec3d6de356f37e4f43d3d11c
Vongkong <dengshenyan@dengsheyandeMBP.lan>
2018-07-24 22:42:33 +0800

    add code

--- src/practice-3.c
+++ src/practice-3.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-
+// 7-4 输出带框文字
 int main(int argc, char **argv) {
+	printf("************\n");
+    printf("  Welcome\n");
+    printf("************\n");
 
     return 0;
 }
--- src/practice-4.c
+++ src/practice-4.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-// 7-4 输出带框文字
+// 7-3 输出菱形图案
 int main(int argc, char **argv) {
-    printf("************\n");
-    printf("  Welcome\n");
-    printf("************\n");
+    printf("  A\n");
+    printf("A   A\n");
+    printf("  A\n");
     
     return 0;
 }
