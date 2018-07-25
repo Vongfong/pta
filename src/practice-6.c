4d9307bc29fd65887ea1869dd10cc0fd6395c669
Vongkong <dengshenyan@dengsheyandeMBP.lan>
2018-07-25 22:00:07 +0800

    six code

---
+++ src/practice-6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// 7-13 输出倒三角图案
+int main(int argc, char **argv) {
+    printf("* * * *\n");
+    printf(" * * *\n");
+    printf("  * *\n");
+    printf("   *\n");
+
+    return 0;
+}
